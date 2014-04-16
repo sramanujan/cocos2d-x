f03e1107c9cb2aab4bcfb53adc8390917dc282d0
Puspesh <krpuspesh@gmail.com>
2014-04-16 21:37:04 +0530

    Fixing crash #Crittercism 98b5b02b6c19f700f8eda8df34ff15bd91eec97fca8ca8aeb9ea78df

--- cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxRenderer.cpp
+++ cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxRenderer.cpp
@@ -15,7 +15,9 @@ extern "C" {
     }
 
     JNIEXPORT void JNICALL Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnPause() {
-        CCApplication::sharedApplication()->applicationDidEnterBackground();
+        if (CCDirector::sharedDirector()->getOpenGLView()) {
+			CCApplication::sharedApplication()->applicationDidEnterBackground();
+		}
 
         CCNotificationCenter::sharedNotificationCenter()->postNotification(EVENT_COME_TO_BACKGROUND, NULL);
     }
