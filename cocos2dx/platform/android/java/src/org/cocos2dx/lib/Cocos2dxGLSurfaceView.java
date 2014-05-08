bb0b103375115999072a07e6667cd6b9755c8f9a
Gitesh <gitesh.iit@gmail.com>
2014-05-08 19:02:28 +0530

    Android Flickering Bug Fix

--- cocos2dx/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos2dx/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -159,6 +159,8 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
 	public void onResume() {
 		super.onResume();
 
+		this.setRenderMode(RENDERMODE_CONTINUOUSLY);
+
 		this.queueEvent(new Runnable() {
 			@Override
 			public void run() {
@@ -175,6 +177,8 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
 				Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleOnPause();
 			}
 		});
+
+		this.setRenderMode(RENDERMODE_WHEN_DIRTY);
 
 		//super.onPause();
 	}
